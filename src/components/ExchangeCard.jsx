00fd0c3ed33d1182aea92c669760cd747f3e64b9
alexisabels <isabelsanchezalex@gmail.com>
2024-02-16 19:34:17 +0100

    test

--- src/components/ExchangeCard.jsx
+++ src/components/ExchangeCard.jsx
@@ -1,6 +1,6 @@
 import { Text, View } from 'react-native'
 import React, { Component } from 'react'
-
+//exchangecard
 export default class ExchangeCard extends Component {
   render() {
     return (
